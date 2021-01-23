a03282d9b11cfb90d2cffa77e866b8f116a20290
IgorHenriqueSouza <br.igorhenrique@gmail.com>
2021-01-23 16:59:15 -0300

    Portfolio começo

---
+++ src/page/landing/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './styles.css'
+
+
+
+function Landing () {
+    return (
+        <div className="landing-page">
+            <div className="name">
+                <h1>IgorSouzaDev</h1>
+            </div>
+            <div className="who-knows">
+                <h2>
+                    Quem sou ?
+                </h2>
+                <p>
+                    Meu nome é Igor Henrique Diniz Souza, tenho 23 anos sou formado em ciencias da computação
+                </p>
+            </div>
+            
+        </div>
+    );
+    
+}
+
+export default Landing;
